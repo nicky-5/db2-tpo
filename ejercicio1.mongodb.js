c4ec2b0a4ad916a6bda143f0acea7e9ad9855198
Francisco F <85972930+FranciscoFerrutti@users.noreply.github.com>
2024-05-14 18:40:05 +0000

    code for exercise 1

---
+++ ejercicio1.mongodb.js
@@ -0,0 +1,34 @@
+use('ej1');
+db.getCollection('albumlist').aggregate([
+    {
+        $group: {
+            _id: "$Year",
+            cantidad: {$sum: 1}
+        }
+    },
+    {
+        $sort: {
+            cantidad: -1
+        }
+    }
+]);
+
+db.getCollection('albumlist').updateMany(
+    {},
+    [{
+        $set: {
+            score: {
+                $subtract: [501, "$Number"]
+            }
+        }
+    }]
+);
+
+db.getCollection('albumlist').find(
+    {},
+    {
+        _id: 0,
+        Artist: 1,
+        score: 1
+    }
+);
